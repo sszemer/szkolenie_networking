6dc97110741423b1546cfb08a08c43fe4f38b5d1
sszemer <sszemer@gmail.com>
2019-10-23 16:37:17 +0200

    obsluga wielu clientow

--- src/com/company/server/MainServer.java
+++ src/com/company/server/MainServer.java
@@ -3,16 +3,17 @@ package com.company.server;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class MainServer {
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(1337);
+        ExecutorService executorService = Executors.newCachedThreadPool();
 
         while (true){
             Socket acceptedSocket = serverSocket.accept();
-            ExecutorService executorService = Executors.newCachedThreadPool();
             executorService.submit(new ServerThread(acceptedSocket));
         }
     }
