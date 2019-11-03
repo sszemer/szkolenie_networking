80c8f5749b7384df35d1ca98623d87c08d3fd9dc
sszemer <sszemer@gmail.com>
2019-11-03 12:01:46 +0100

    homework

--- src/main/java/nbp/WalutyMain.java
+++ src/main/java/nbp/WalutyMain.java
@@ -17,40 +17,37 @@ import java.util.List;
 
 public class WalutyMain {
 
+    private static boolean found;
+
     public static void main(String[] args) throws IOException {
-        List<Rate> merge=null;
 
         Tables[] tablesA  = callWS("A");
         Tables[] tablesB  = callWS("B");
 
-        boolean found=true;
+        System.out.println(tablesB[0].getRates().size());
+
         for (Tables tables : tablesA) {
             tables.getRates().forEach(rate -> {
                 for (Tables tables2 : tablesB) {
+                    found=true;
                     tables2.getRates().forEach(rate1 -> {
                         if (!rate.getCode().equalsIgnoreCase(rate1.getCode())) {
-                            //jak sie nie uda to skopiowac do b
+                            //jak nie znajdzie to skopiowac do b
                             found = false;
                         }
                         //albo jak znajdzie to usunac z A a potem merge
-                            tables2.getRates().add(rate);
                     });
+                            tables2.getRates().add(rate);
                 }
             });
         }
-//            tableNames = tableNames.substring(1, tableNames.length()-1);
-//            Tables tables = objectMapper.readValue(tableNames, Tables.class);
-//            System.out.println(tables.getRates().get(0).getCurrency());
-//            System.out.println(tables.getRates().get(0).getCode());
-//            System.out.println(tables.getRates().get(0).getMid());
-//            BigDecimal pln = BigDecimal.valueOf(100.00);
-//            System.out.println(objectMapper.writeValueAsString(tableNames));
-//            System.out.println(pln.divide(tables.getRates().get(0).getMid(), RoundingMode.HALF_EVEN));
-//        } catch (MalformedURLException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
+        for (Tables tables : tablesB) {
+            tables.getRates().stream().forEach(rate -> {
+                System.out.println(rate.getCode());
+                System.out.println(tables.getRates().size());
+            });
         }
+    }
 
 
     private static Tables[] callWS(String table) throws IOException {
